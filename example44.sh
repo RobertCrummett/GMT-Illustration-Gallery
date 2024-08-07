afd5c5a5438434f1b17a69b86d027841391ef3ec
Robert Crummett <rncrummett5@gmail.com>
2024-08-07 12:23:06 -0600

    Example 47 regression in GMT

--- example44.sh
+++ example44.sh
@@ -12,7 +12,7 @@ gmt coast -R10W/5E/35N/44N -JM15c -EES+gbisque -Gbrown -Wfaint -N1/1p -Sazure \
 	-Dh --FORMAT_GEO_MAP=dddF -c0
 gmt inset begin -DjTR+w5c/2.35c+o0.4c/0.25c -F+gwhite+p1p+c0.1c+s
 gmt coast -R15W/35E/30N/48N -JM? -Da -Gbrown -B0 -EES+gbisque \
-	 --MAP_FRAME_TYPE=plain
+	--MAP_FRAME_TYPE=plain
 gmt inset end
 gmt coast -R110E/170E/44S/9S -JM15c -Wfaint -N2/1p -EAU+gbisque -Gbrown \
 	-Sazure1 -Dh --FORMAT_GEO_MAP=dddF -c1
